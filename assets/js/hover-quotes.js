659c98cdef12909c03ff616e517320e11d6e5b44
Dalton Menezes <daltonmenezes@outlook.com>
2016-11-15 22:40:59 -0200

    Update to float animation

--- assets/js/hover-quotes.js
+++ assets/js/hover-quotes.js
@@ -6,57 +6,57 @@ var about = document.getElementById('about');
 var contact = document.getElementById('contact');
 
 blog.addEventListener('mouseover', function() {
-  topQuotes.style.animation = 'quote-start-transition 0.8s ease';
-  bottomQuotes.style.animation = 'quote-start-transition 0.8s ease';
+  topQuotes.style.animation = 'float 2s infinite, quote-start-transition 0.8s ease';
+  bottomQuotes.style.animation = 'float 2s infinite, quote-start-transition 0.8s ease';
   topQuotes.style.backgroundPosition = '0 -50px';
   bottomQuotes.style.backgroundPosition = '0 -150px';
 });
 
 blog.addEventListener('mouseout', function() {
-  topQuotes.style.animation = 'quote-finish-transition 0.8s ease';
-  bottomQuotes.style.animation = 'quote-finish-transition 0.8s ease';
+  topQuotes.style.animation = 'float 2s infinite, quote-finish-transition 0.8s ease';
+  bottomQuotes.style.animation = 'float 2s infinite, quote-finish-transition 0.8s ease';
   topQuotes.style.backgroundPosition = '0 0';
   bottomQuotes.style.backgroundPosition = '0 -200px';
 });
 
 projects.addEventListener('mouseover', function() {
-  topQuotes.style.animation = 'quote-start-transition 0.8s ease';
-  bottomQuotes.style.animation = 'quote-start-transition 0.8s ease';
+  topQuotes.style.animation = 'float 2s infinite, quote-start-transition 0.8s ease';
+  bottomQuotes.style.animation = 'float 2s infinite, quote-start-transition 0.8s ease';
   topQuotes.style.backgroundPosition = '0 -100px';
   bottomQuotes.style.backgroundPosition = '0 -100px';
 });
 
 projects.addEventListener('mouseout', function() {
-  topQuotes.style.animation = 'quote-finish-transition 0.8s ease';
-  bottomQuotes.style.animation = 'quote-finish-transition 0.8s ease';
+  topQuotes.style.animation = 'float 2s infinite, quote-finish-transition 0.8s ease';
+  bottomQuotes.style.animation = 'float 2s infinite, quote-finish-transition 0.8s ease';
   topQuotes.style.backgroundPosition = '0 0';
   bottomQuotes.style.backgroundPosition = '0 -200px';
 });
 
 about.addEventListener('mouseover', function() {
-  topQuotes.style.animation = 'quote-start-transition 0.8s ease';
-  bottomQuotes.style.animation = 'quote-start-transition 0.8s ease';
+  topQuotes.style.animation = 'float 2s infinite, quote-start-transition 0.8s ease';
+  bottomQuotes.style.animation = 'float 2s infinite, quote-start-transition 0.8s ease';
   topQuotes.style.backgroundPosition = '0 -150px';
   bottomQuotes.style.backgroundPosition = '0 -50px';
 });
 
 about.addEventListener('mouseout', function() {
-  topQuotes.style.animation = 'quote-finish-transition 0.8s ease';
-  bottomQuotes.style.animation = 'quote-finish-transition 0.8s ease';
+  topQuotes.style.animation = 'float 2s infinite, quote-finish-transition 0.8s ease';
+  bottomQuotes.style.animation = 'float 2s infinite, quote-finish-transition 0.8s ease';
   topQuotes.style.backgroundPosition = '0 0';
   bottomQuotes.style.backgroundPosition = '0 -200px';
 });
 
 contact.addEventListener('mouseover', function() {
-  topQuotes.style.animation = 'quote-start-transition 0.8s ease';
-  bottomQuotes.style.animation = 'quote-start-transition 0.8s ease';
+  topQuotes.style.animation = 'float 2s infinite, quote-start-transition 0.8s ease';
+  bottomQuotes.style.animation = 'float 2s infinite, quote-start-transition 0.8s ease';
   topQuotes.style.backgroundPosition = '0 -200px';
   bottomQuotes.style.backgroundPosition = '0 0';
 });
 
 contact.addEventListener('mouseout', function() {
-  topQuotes.style.animation = 'quote-finish-transition 0.8s ease';
-  bottomQuotes.style.animation = 'quote-finish-transition 0.8s ease';
+  topQuotes.style.animation = 'float 2s infinite, quote-finish-transition 0.8s ease';
+  bottomQuotes.style.animation = 'float 2s infinite, quote-finish-transition 0.8s ease';
   topQuotes.style.backgroundPosition = '0 0';
   bottomQuotes.style.backgroundPosition = '0 -200px';
 });
