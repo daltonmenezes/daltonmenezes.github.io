1297d761d8f1d3728d86c62f243b692ae266f64a
Dalton Menezes <daltonmenezes@outlook.com>
2016-11-16 13:53:35 -0200

    Add about.addEventListener to open about modal window and update the the logic of close modal window button

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,12 +1,21 @@
-var close = document.querySelector('.close');
+var close = document.querySelectorAll('.close');
 var modal = document.querySelector('.modal');
+var modalAbout = document.getElementById('modal-about');
 
 contact.addEventListener('click', function() {
   modal.style.visibility = 'visible';
   modal.style.animation = 'quote-start-transition 0.5s ease';
 });
 
-close.addEventListener('click', function() {
-  modal.style.animation = 'quote-finish-transition 0.8s ease';
-  modal.style.visibility = 'hidden';
+about.addEventListener('click', function() {
+  modalAbout.style.visibility = 'visible';
+  modalAbout.style.animation = 'quote-start-transition 0.5s ease';
 });
+
+for (i = 0; i < close.length; i++) {
+    close[i].addEventListener('click', function(event) {
+      _window = event.toElement.offsetParent;
+      _window.style.animation = 'quote-finish-transition 0.8s ease';
+      _window.style.visibility = 'hidden';
+    });
+}
