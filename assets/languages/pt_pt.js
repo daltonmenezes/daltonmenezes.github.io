ead39697c0f73b3d2aecbb6ee5d3d0e56bc01ea3
Dalton Menezes <daltonmenezes@outlook.com>
2016-11-23 04:22:29 -0200

    Add pt_pt language pack

---
+++ assets/languages/pt_pt.js
@@ -0,0 +1,20 @@
+pt_pt = {
+  _language: 'pt_pt',
+  english: 'INGLÊS',
+  portuguese: 'PORTUGUÊS',
+  projects: 'PROJETOS',
+  about: 'SOBRE',
+  contact: 'CONTATO',
+  hi: 'Olá, sou Dalton Menezes',
+  about_text_paragraph_one: 'Tenho 27 anos, sou Brasileiro, nasci na Bahia e vivo no Rio de Janeiro desde 2009. Sou Designer, Web Designer e Desenvolvedor de Software Independente. Amo a comunidade de software livre e open source e contribuo com ela sempre que posso.',
+  about_text_paragraph_two: 'Também sou escritor e músico. Espero terminar em breve os meus trabalhos e mostrar para vocês.<br>A baixo há algumas das minhas habilidades que você pode conferir.',
+  orb_description: 'Seu amigável manipulador JavaScript de idiomas. O ORB transforma rapida e facilmente o seu website/projeto em multilíngue.',
+  pepper_description: 'Um jogo simples, gratuito e de código aberto parecido com Flappy Bird escrito com HTML5 e JavaScript.',
+  yout_description: 'Yout Player é a nova maneira de assistir suas playlists do YouTube no Desktop. Sem comerciais durante os vídeos, simples de usar e possui janela flutuante. Disponível gratuitamente para Linux, Mac e Windows.',
+  see_all_software: 'Ver todos os projetos sobre software',
+  art_for_sale1: 'Arte para venda.',
+  art_for_sale2: 'Arte para venda.',
+  art_for_sale3: 'Arte para venda.',
+  art_for_roberbills: 'Arte para a banda Roberbills.',
+  see_all_design: 'Ver todos os projetos sobre design'
+};
