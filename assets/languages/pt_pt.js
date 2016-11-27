1c460d5a0fd371060ab50b316043320c7a6e55cd
Dalton Menezes <daltonmenezes@outlook.com>
2016-11-27 00:01:09 -0200

    Update texts about UNI

--- assets/languages/pt_pt.js
+++ assets/languages/pt_pt.js
@@ -8,7 +8,7 @@ pt_pt = {
   hi: 'Olá, sou Dalton Menezes',
   about_text_paragraph_one: 'Tenho 27 anos, sou Brasileiro, nasci na Bahia e vivo no Rio de Janeiro desde 2009. Sou Designer, Web Designer e Desenvolvedor de Software Independente. Amo a comunidade de software livre e open source e contribuo com ela sempre que posso.',
   about_text_paragraph_two: 'Também sou escritor e músico. Espero terminar em breve os meus trabalhos e mostrar para vocês.<br>A baixo há algumas das minhas habilidades que você pode conferir.',
-  uni_description: 'Uma interface universal para gerenciadores de pacote em distribuições Linux.',
+  uni_description: 'Uma interface universal para gerenciadores de pacotes em distribuições Linux. UNI é uma camada de abstração que lhe permite gerenciar gerenciadores de pacotes diferentes usando os mesmos comandos UNI independente da distribuição que você esteja usando e automatiza as tarefas repetitivas deles.',
   orb_description: 'Seu amigável manipulador JavaScript de idiomas. O ORB transforma rapida e facilmente o seu website/projeto em multilíngue.',
   pepper_description: 'Um jogo simples, gratuito e de código aberto parecido com Flappy Bird escrito com HTML5 e JavaScript.',
   yout_description: 'Yout Player é a nova maneira de assistir suas playlists do YouTube no Desktop. Sem comerciais durante os vídeos, simples de usar e possui janela flutuante. Disponível gratuitamente para Linux, Mac e Windows.',
