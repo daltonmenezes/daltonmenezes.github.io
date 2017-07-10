a6629dd44e95d6c7a07fb8e6e610f8e3d07b4c3d
Dalton Menezes <daltonmenezes@outlook.com>
2017-07-10 20:50:50 -0300

    Update book art projects

--- assets/languages/en.js
+++ assets/languages/en.js
@@ -18,5 +18,6 @@ en = {
   see_all_books: 'See all book artworks',
   see_all_bands: 'See all band artworks',
   area_militar_info: 'Book: Área Militar<br/>Author: Nathany Teixeira<br/>Release: 2017<br/>Buy the Book: Soon',
-  as_fases_da_lua_info: 'Book: As Fases da Lua<br/>Author: Cínthia Sampaio<br/>Release: 2017<br/>Buy the Book: Soon' 
+  as_fases_da_lua_info: 'Book: As Fases da Lua<br/>Author: Cínthia Sampaio<br/>Release: 2017<br/>Buy the Book: Soon',
+  city_in_the_blue_mountains_info: 'Book: The City in the Blue Mountains<br/>Author: Josephine Miller<br/>Release: 2017<br/>Buy the Book: Soon'  
 };
