91e4387c7a97ba15c227b9bad4de608694788118
Dalton Menezes <daltonmenezes@outlook.com>
2017-09-25 22:37:02 -0300

    Add new personal photo and quote color

--- assets/js/hover-quotes.js
+++ assets/js/hover-quotes.js
@@ -1,3 +1,4 @@
+var photo = document.getElementById('photo');
 var topQuotes = document.getElementById('top-quotes');
 var bottomQuotes = document.getElementById('bottom-quotes');
 var blog = document.getElementById('blog');
@@ -11,6 +12,7 @@ blog.addEventListener('mouseover', function() {
   bottomQuotes.style.animation = 'float 2s infinite, quote-start-transition 0.8s ease';
   topQuotes.style.backgroundPosition = '0 -100px';
   bottomQuotes.style.backgroundPosition = '0 -100px';
+  photo.style.backgroundPosition = '-620px 0';
 });
 
 blog.addEventListener('mouseout', function() {
@@ -18,13 +20,15 @@ blog.addEventListener('mouseout', function() {
   bottomQuotes.style.animation = 'float 2s infinite, quote-finish-transition 0.8s ease';
   topQuotes.style.backgroundPosition = '0 0';
   bottomQuotes.style.backgroundPosition = '0 -200px';
+  photo.style.backgroundPosition = '0 0';
 });
 
 software.addEventListener('mouseover', function() {
   topQuotes.style.animation = 'float 2s infinite, quote-start-transition 0.8s ease';
   bottomQuotes.style.animation = 'float 2s infinite, quote-start-transition 0.8s ease';
-  topQuotes.style.backgroundPosition = '0 -50px';
-  bottomQuotes.style.backgroundPosition = '0 -150px';
+  topQuotes.style.backgroundPosition = '0 -250px';
+  bottomQuotes.style.backgroundPosition = '0 -250px';
+  photo.style.backgroundPosition = '-1550px 0';
 });
 
 software.addEventListener('mouseout', function() {
@@ -32,6 +36,7 @@ software.addEventListener('mouseout', function() {
   bottomQuotes.style.animation = 'float 2s infinite, quote-finish-transition 0.8s ease';
   topQuotes.style.backgroundPosition = '0 0';
   bottomQuotes.style.backgroundPosition = '0 -200px';
+  photo.style.backgroundPosition = '0 0';
 });
 
 design.addEventListener('mouseover', function() {
@@ -39,6 +44,7 @@ design.addEventListener('mouseover', function() {
   bottomQuotes.style.animation = 'float 2s infinite, quote-start-transition 0.8s ease';
   topQuotes.style.backgroundPosition = '0 -50px';
   bottomQuotes.style.backgroundPosition = '0 -150px';
+  photo.style.backgroundPosition = '-310px 0';
 });
 
 design.addEventListener('mouseout', function() {
@@ -46,6 +52,7 @@ design.addEventListener('mouseout', function() {
   bottomQuotes.style.animation = 'float 2s infinite, quote-finish-transition 0.8s ease';
   topQuotes.style.backgroundPosition = '0 0';
   bottomQuotes.style.backgroundPosition = '0 -200px';
+  photo.style.backgroundPosition = '0 0';
 });
 
 about.addEventListener('mouseover', function() {
@@ -53,6 +60,7 @@ about.addEventListener('mouseover', function() {
   bottomQuotes.style.animation = 'float 2s infinite, quote-start-transition 0.8s ease';
   topQuotes.style.backgroundPosition = '0 -150px';
   bottomQuotes.style.backgroundPosition = '0 -50px';
+  photo.style.backgroundPosition = '-1240px 0';
 });
 
 about.addEventListener('mouseout', function() {
@@ -60,6 +68,7 @@ about.addEventListener('mouseout', function() {
   bottomQuotes.style.animation = 'float 2s infinite, quote-finish-transition 0.8s ease';
   topQuotes.style.backgroundPosition = '0 0';
   bottomQuotes.style.backgroundPosition = '0 -200px';
+  photo.style.backgroundPosition = '0 0';
 });
 
 contact.addEventListener('mouseover', function() {
@@ -67,6 +76,7 @@ contact.addEventListener('mouseover', function() {
   bottomQuotes.style.animation = 'float 2s infinite, quote-start-transition 0.8s ease';
   topQuotes.style.backgroundPosition = '0 -200px';
   bottomQuotes.style.backgroundPosition = '0 0';
+  photo.style.backgroundPosition = '-930px 0';
 });
 
 contact.addEventListener('mouseout', function() {
@@ -74,4 +84,5 @@ contact.addEventListener('mouseout', function() {
   bottomQuotes.style.animation = 'float 2s infinite, quote-finish-transition 0.8s ease';
   topQuotes.style.backgroundPosition = '0 0';
   bottomQuotes.style.backgroundPosition = '0 -200px';
+  photo.style.backgroundPosition = '0 0';
 });
