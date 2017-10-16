b11fc7505c3a8781c7f610ac3faeca95928299ec
Dalton Menezes <daltonmenezes@outlook.com>
2017-10-16 16:43:07 -0200

    Add support to navigate from go back and go foward history on browser

--- assets/js/modal-active-url.js
+++ assets/js/modal-active-url.js
@@ -1,8 +1,25 @@
-var hash = window.location.hash;
-
-switch (hash) {
-  case '#contact': openModalContactWindow(); break;
-  case '#about': openModalAboutWindow(); break;
-  case '#software': openModalSoftwareWindow(); break;
-  case '#design': openModalDesignWindow(); break;
+function clearModal() {
+  var modalList = document.querySelectorAll('.modal');
+  for (i = 0; i < modalList.length; i++) {
+      modalList[i].style.animation = 'quote-finish-transition 0.8s ease';
+      modalList[i].style.visibility = 'hidden';
+  }  
 }
+
+function openModal() {
+  var hash = window.location.hash;
+  switch (hash) {
+    case '#contact': openModalContactWindow(); break;
+    case '#about': openModalAboutWindow(); break;
+    case '#software': openModalSoftwareWindow(); break;
+    case '#design': openModalDesignWindow(); break;
+  }
+}
+
+openModal();
+
+window.addEventListener("hashchange", function() {
+  clearModal();
+  openModal();
+}, false);
+
