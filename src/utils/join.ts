355440bd94ab66f90c61525152acc6348e100bcd
Dalton Menezes <daltonmenezes@outlook.com>
2020-11-21 00:28:26 -0300

    Add isArray check to join function

--- src/utils/join.ts
+++ src/utils/join.ts
@@ -3,6 +3,10 @@
  * @param array An array used to be joinned as an string basead on a separator.
  * @param separator A string used to separate one element of an array from the next in the resulting String. If omitted, the array elements are separated with a white space.
  */
-export default function join(array: unknown[], separator = ' '): string {
+export default function join(array: string[], separator = ' '): string {
+  if (!Array.isArray(array)) {
+    return ''
+  }
+
   return array.join(separator)
 }
