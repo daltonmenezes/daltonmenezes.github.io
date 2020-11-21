00c7a59aca8227f41f39e493e151696e0c3677dd
Dalton Menezes <daltonmenezes@outlook.com>
2020-11-21 00:59:45 -0300

    Add margin to Header and Menu on App

--- src/screens/App/index.tsx
+++ src/screens/App/index.tsx
@@ -2,12 +2,14 @@ import React, { ReactElement } from 'react'
 import Header from 'components/Layout/Header'
 import Menu, { Item } from 'components/Layout/Menu'
 
+import styles from './styles.module.sass'
+
 export default function App(): ReactElement {
   return (
     <div className="App">
-      <Header>
+      <Header classNames={[styles.header]}>
         <h1>DALTON MENEZES</h1>
-        <Menu horizontal withBullets>
+        <Menu horizontal withBullets classNames={[styles.menu]}>
           <Item>COLABORADORES</Item>
           <Item>LITERATURA</Item>
           <Item>SOFTWARE</Item>
