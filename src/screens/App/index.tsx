7693f15d1bff31a4a41297f7312c85327d0a4ffc
Dalton Menezes <daltonmenezes@outlook.com>
2020-11-21 23:29:05 -0300

    Update App to use useMatchMedia to handle when horizontal and withBullets props should be true

--- src/screens/App/index.tsx
+++ src/screens/App/index.tsx
@@ -1,16 +1,23 @@
 import React, { ReactElement } from 'react'
+import { useMatchMedia } from 'hooks'
 import { Header, Title } from 'components/Layout'
 import Menu, { Item } from 'components/Layout/Menu'
 
 import styles from './styles.module.sass'
 
 export default function App(): ReactElement {
+  const matches = useMatchMedia('max-width: 500px')
+
   return (
     <div className="App">
       <Header classNames={[styles.header]}>
         <Title>DALTON MENEZES</Title>
 
-        <Menu horizontal withBullets classNames={[styles.menu]}>
+        <Menu
+          horizontal={!matches}
+          withBullets={!matches}
+          classNames={[styles.menu]}
+        >
           <Item to="/supporters">COLABORADORES</Item>
           <Item to="/literature">LITERATURA</Item>
           <Item to="/software">SOFTWARE</Item>
