22b3a5b23304a3069aefc98ee583f4a31836ca5c
Dalton Menezes <daltonmenezes@outlook.com>
2020-11-25 21:18:12 -0300

    Update Link component to work with internal and external URLs

--- src/components/Layout/Link/index.tsx
+++ src/components/Layout/Link/index.tsx
@@ -1,14 +1,42 @@
 import React, { PropsWithChildren, ReactElement } from 'react'
 import { Link as HyperLink, LinkProps } from 'react-router-dom'
+import join from 'utils/join'
+
+export interface LinkDef extends LinkProps {
+  classNames?: (string | string[])[]
+}
 
 export default function Link({
   to,
   children,
+  classNames,
   ...rest
-}: PropsWithChildren<LinkProps>): ReactElement {
-  return (
-    <HyperLink to={to} {...rest} replace>
-      {children}
-    </HyperLink>
-  )
+}: PropsWithChildren<LinkDef>): ReactElement {
+  const URL = String(to || '')
+  const isInternalLink = URL.startsWith('/') || !to
+  const classes = join(classNames ? classNames?.flat() : [])
+
+  function renderInternalLink() {
+    return (
+      <HyperLink to={URL} className={classes} {...rest} replace>
+        {children}
+      </HyperLink>
+    )
+  }
+
+  function renderExternalLink() {
+    return (
+      <a
+        href={URL}
+        target="_blank"
+        rel="noreferrer"
+        className={classes}
+        {...rest}
+      >
+        {children}
+      </a>
+    )
+  }
+
+  return isInternalLink ? renderInternalLink() : renderExternalLink()
 }
